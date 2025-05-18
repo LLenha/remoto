d45e49211acf7eb6153edd94075bf9c64f02a943
LLenha <lm.lenha@gmail.com>
2025-05-18 21:27:30 +0000

    adicionando gasolina.py

--- gasolina.py
+++ gasolina.py
@@ -7,4 +7,8 @@ gasolina = data[['dia', 'venda']].groupby('dia').mean().reset_index()
 
 with sns.axes_style('whitegrid'):
   grafico = sns.lineplot(data=gasolina, x='dia', y='venda')
+  grafico.set_title('Preço médio da gasolina', fontsize = 10, fontweight = 'bold')
+  grafico.set_xlabel('Dia', fontsize = 10)
+  grafico.set_ylabel('Preço', fontsize = 10)
+ 
   grafico.figure.savefig('gasolina.png')
